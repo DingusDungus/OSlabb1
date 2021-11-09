03c0cfbd782b636165fdfc3405c78af854ab3ea8
Noah <nallephu12@gmail.com>
2021-11-09 11:43:43 +0100

    Added file that has sleep but no array/buffer

--- task2/task2-nobuffer.cpp
+++ task2/task2-nobuffer.cpp
@@ -9,13 +9,13 @@
 #define SHM_R 0400
 #define SHM_W 0200
 
+// No buffer
+
 int main(int argc, char** argv){
     struct shm_struct{
-        int buffer[10];
+        int buffer;
         unsigned empty;
-        unsigned sent;
     };
-    unsigned index;
     float randomSleep = 0;
     volatile struct shm_struct* shmp = NULL;
     char* addr = NULL;
@@ -28,27 +28,18 @@ int main(int argc, char** argv){
     /* allocate a chunk of shared memory */
     shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
     shmp = (struct shm_struct*)shmat(shmid, addr, 0);
-    index = 0;
-    shmp->empty = 1;
-    shmp->sent = 0;
+    shmp->empty = 0;
     pid = fork();
-
     if (pid != 0){
         /* here's the parent, acting as producer */
         while (var1 < 100){
-            if (index > 9){
-                index = 0;
-            }
-
             /* write to shmem */
             var1++;
-            while (shmp->empty == 0);
-            printf("Sending %d to index %d\n", var1, index);
+            while (shmp->empty == 1); /* busy wait until the buffer is empty */
+            shmp->buffer = var1;
+            shmp->empty = 1;
+            printf("Sending %d\n", var1);
             fflush(stdout);
-            shmp->buffer[index] = var1;
-            shmp->sent++;
-            index++;
-
             randomSleep = rand() % 20 + 1;
             sleep((randomSleep / 10));
         }
@@ -57,32 +48,17 @@ int main(int argc, char** argv){
     }
     else{
         /* here's the child, acting as consumer */
-        unsigned recieved = 0;
         while (var2 < 100){
-            while ((shmp->sent - recieved) <= 0);
             /* read from shmem */
-            var2 = shmp->buffer[index];
-            printf("Received %d from index %d\n", var2, index);
+            while (shmp->empty == 0); /* busy wait until there is something */
+            var2 = shmp->buffer;
+            shmp->empty = 0;
+            printf("Received %d\n", var2);
             fflush(stdout);
-            recieved++;
-            index++;
-
-            if (index > 9){
-                index = 0;
-            }
-            if (shmp->empty == 0 && (shmp->sent - recieved) > 10){
-                index = 0;
-                shmp->empty = 0;
-            }
-            else{
-                shmp->empty = 1;
-            }
-
             randomSleep = rand() % 20 + 1;
             sleep((randomSleep / 10));
         }
         shmdt(addr);
         shmctl(shmid, IPC_RMID, shm_buf);
     }
-    return 0;
 }
