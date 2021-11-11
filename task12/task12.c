af84aaa4212c7c73ae7b1f6937edad0c430fa840
Noah <nallephu12@gmail.com>
2021-11-11 15:43:51 +0100

    Philosophers are now aware of where the sticks they can pick up are.

--- task12/task12.c
+++ task12/task12.c
@@ -1,9 +1,8 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
-struct threadArgs
-{
+struct threadArgs {
     int id;
     /*int leftGrabbed;*/
     /*int rightGrabbed;*/
@@ -14,14 +13,27 @@ pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // philosopher/thread logic
 void logic(unsigned long id) {
-    unsigned int leftGrabbed = 0; // set to 0, no chopstick.
-    unsigned int rightGrabbed = 0; // set to 0, no chopstick.
-    unsigned int hasEaten = 0; // set to 0, has not eaten.
-    int pos = id+1;
+    unsigned int leftGrabbed = 0;   // set to 0, no chopstick.
+    unsigned int rightGrabbed = 0;  // set to 0, no chopstick.
+    unsigned int hasEaten = 0;      // set to 0, has not eaten.
+    int pos = id;
+    int leftStickPos;
+    int rightStickPos;
+    if (pos == 4) {
+        rightStickPos = 0;
+    } else {
+        rightStickPos = pos + 1;
+    }
+    if (pos == 0) {
+        leftStickPos = 4;
+    } else {
+        leftStickPos = pos - 1;
+    }
     pthread_mutex_lock(&lock);
-    printf("Im philosopher %d\n", pos);
+    printf("Im philosopher %d, ", pos);
+    printf("My left stick pos is %d My right stick pos is %d\n", leftStickPos,rightStickPos);
     while (hasEaten == 0) {
-
+        break;
     }
     pthread_mutex_unlock(&lock);
 }
@@ -34,7 +46,7 @@ void* child(void* buf) {
 }
 
 int main(int argc, char** argv) {
-    pthread_t *children;
+    pthread_t* children;
     unsigned long id = 1;
     unsigned long nThreads = 5;
 
@@ -42,18 +54,18 @@ int main(int argc, char** argv) {
     // initialize array with chopsticks.
     // chopstick = 1, no chopstick = 0
     for (int i = 0; i < 5; i++) {
-      chopsticks[i] = 1;
-      printf("testtest \n");
+        chopsticks[i] = 1;
+        printf("testtest \n");
     }
     struct threadArgs* args;
 
-    children = malloc( nThreads * sizeof(pthread_t) );
+    children = malloc(nThreads * sizeof(pthread_t));
     for (id = 1; id < nThreads; id++)
-        pthread_create(&(children[id-1]), NULL, child, (void*)id);
-    logic(0); // main thread work (id=1)
-      printf("after logic \n");
+        pthread_create(&(children[id - 1]), NULL, child, (void*)id);
+    logic(0);  // main thread work (id=1)
+    printf("after logic \n");
     for (id = 1; id < nThreads; id++)
-        pthread_join(children[id-1], NULL);
+        pthread_join(children[id - 1], NULL);
     printf("\nNumber of threads is %lu", nThreads);
     free(children);
     pthread_mutex_destroy(&lock);
