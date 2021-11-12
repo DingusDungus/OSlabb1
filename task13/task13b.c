373c98dd151d21b1e8ea2a92ffa2d74091df8dcd
Gabriel Ivarsson <gabriel@pop-os.localdomain>
2021-11-12 16:39:57 +0100

    Finioshed task 13

--- task13/task13b.c
+++ task13/task13b.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>  /* time */
-#include <fcntl.h> /* For O_* constants */
-#include <semaphore.h>
+#include <time.h> /* time */
 #include <unistd.h>
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -19,12 +17,13 @@ struct threadArgs
 
 struct tableContent
 {
-    int locked;
     int stickGrabbed;
+    int reserved;
 };
 
 void printChops(struct tableContent *chopsticks)
 {
+    pthread_mutex_lock(&lock);
     for (int i = 0; i < 5; i++)
     {
         printf("%d", chopsticks[i].stickGrabbed);
@@ -32,6 +31,7 @@ void printChops(struct tableContent *chopsticks)
     }
     printf("\n");
     fflush(stdout);
+    pthread_mutex_unlock(&lock);
 }
 
 void initChopsticks(struct tableContent *chopsticks)
@@ -39,7 +39,7 @@ void initChopsticks(struct tableContent *chopsticks)
     for (int i = 0; i < 5; i++)
     {
         chopsticks[i].stickGrabbed = 1;
-        chopsticks[i].locked = 0;
+        chopsticks[i].reserved = 0;
     }
 }
 
@@ -51,24 +51,17 @@ void releaseChopsticks(void *params)
     pthread_mutex_lock(&lock);
     if (args->id == 0)
     {
-        args->chopsticks[4].stickGrabbed = 1;
-        args->chopsticks[1].stickGrabbed = 1;
-        args->chopsticks[args->id + 1].locked = 0;
-        args->chopsticks[4].locked = 0;
-    }
-    else if (args->id == 4)
-    {
-        args->chopsticks[3].stickGrabbed = 1;
         args->chopsticks[0].stickGrabbed = 1;
-        args->chopsticks[0].locked = 0;
-        args->chopsticks[args->id - 1].locked = 0;
+        args->chopsticks[4].stickGrabbed = 1;
+        args->chopsticks[0].reserved = 0;
+        args->chopsticks[4].reserved = 0;
     }
     else
     {
         args->chopsticks[args->id - 1].stickGrabbed = 1;
-        args->chopsticks[args->id + 1].stickGrabbed = 1;
-        args->chopsticks[args->id - 1].locked = 0;
-        args->chopsticks[args->id + 1].locked = 0;
+        args->chopsticks[args->id].stickGrabbed = 1;
+        args->chopsticks[args->id - 1].reserved = 0;
+        args->chopsticks[args->id].reserved = 0;
     }
     pthread_mutex_unlock(&lock);
 }
@@ -76,107 +69,76 @@ void releaseChopsticks(void *params)
 int grabLeft(void *params)
 {
     struct threadArgs *args = params;
+    int returnInt = 0;
+    pthread_mutex_lock(&lock);
     if (args->id == 0)
     {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[4].stickGrabbed == 1 && args->chopsticks[args->id + 1].locked == 0)
+
+        if ((args->chopsticks[4].reserved == 0 && args->chopsticks[args->id].reserved == 0))
         {
             args->chopsticks[4].stickGrabbed = 0;
-            args->chopsticks[args->id + 1].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
+            args->chopsticks[4].reserved = 1;
+            args->chopsticks[args->id].reserved = 1;
+            returnInt = 1;
         }
         else
         {
-            pthread_mutex_unlock(&lock);
-            return -1;
-        }
-    }
-    else if (args->id == 4)
-    {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[args->id - 1].stickGrabbed == 1 && args->chopsticks[0].locked == 0)
-        {
-            args->chopsticks[args->id - 1].stickGrabbed = 0;
-            args->chopsticks[0].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
-        }
-        else
-        {
-            pthread_mutex_unlock(&lock);
-            return -1;
+            returnInt = -1;
         }
     }
     else
     {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[args->id - 1].stickGrabbed == 1 && args->chopsticks[args->id + 1].locked == 0)
+        if (args->chopsticks[args->id - 1].reserved == 0 && args->chopsticks[args->id].reserved == 0)
         {
             args->chopsticks[args->id - 1].stickGrabbed = 0;
-            args->chopsticks[args->id + 1].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
+            args->chopsticks[args->id - 1].reserved = 1;
+            args->chopsticks[args->id].reserved = 1;
+            returnInt =  1;
         }
         else
         {
-            pthread_mutex_unlock(&lock);
-            return -1;
+            returnInt = -1;
         }
     }
+    pthread_mutex_unlock(&lock);
+
+    return returnInt;
 }
 
 int grabRight(void *params)
 {
     struct threadArgs *args = params;
+    int returnInt = 0;
+
+    pthread_mutex_lock(&lock);
     if (args->id == 0)
     {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[args->id + 1].stickGrabbed == 1 && args->chopsticks[4].locked == 0)
+
+        if ((args->chopsticks[4].reserved == 1 && args->chopsticks[args->id].reserved == 1))
         {
-            args->chopsticks[args->id + 1].stickGrabbed = 0;
-            args->chopsticks[4].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
+            args->chopsticks[args->id].stickGrabbed = 0;
+            returnInt = 1;
         }
         else
         {
-            pthread_mutex_unlock(&lock);
-            return -1;
-        }
-    }
-    else if (args->id == 4)
-    {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[0].stickGrabbed == 1 && args->chopsticks[args->id - 1].locked == 0)
-        {
-            args->chopsticks[0].stickGrabbed = 0;
-            args->chopsticks[args->id - 1].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
-        }
-        else
-        {
-            pthread_mutex_unlock(&lock);
-            return -1;
+            returnInt = -1;
         }
     }
     else
     {
-        pthread_mutex_lock(&lock);
-        if (args->chopsticks[args->id + 1].stickGrabbed == 1 && args->chopsticks[args->id - 1].locked == 0)
+        if (args->chopsticks[args->id].stickGrabbed == 1 && args->chopsticks[args->id].stickGrabbed == 1)
         {
-            args->chopsticks[args->id + 1].stickGrabbed = 0;
-            args->chopsticks[args->id - 1].locked = 1;
-            pthread_mutex_unlock(&lock);
-            return 1;
+            args->chopsticks[args->id].stickGrabbed = 0;
+            returnInt = 1;
         }
         else
         {
-            pthread_mutex_unlock(&lock);
-            return -1;
+            returnInt = -1;
         }
     }
+    pthread_mutex_unlock(&lock);
+
+    return returnInt;
 }
 
 void *child(void *params)
@@ -225,6 +187,7 @@ void *child(void *params)
     }
     printf("Thread %d lays down its chopsticks on the table\n", args->id);
     fflush(stdout);
+    releaseChopsticks(args);
 
     free(args);
 
