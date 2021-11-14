d21f0e2da853c3071817909e80afdfec5678b490
Noah <nallephu12@gmail.com>
2021-11-14 18:42:00 +0100

    solved task 16 with pthread_barrier_wait

--- task16/task16.c
+++ task16/task16.c
@@ -15,38 +15,21 @@
 static double a[SIZE][SIZE];
 static double b[SIZE][SIZE];
 static double c[SIZE][SIZE];
-
+pthread_barrier_t barrier;
 static void
-init_matrix(void)
+init_matrix(int row)
 {
-    int i, j;
+    int j;
 
-    for (i = 0; i < SIZE; i++)
         for (j = 0; j < SIZE; j++)
         {
             /* Simple initialization, which enables us to easy check
-	         * the correct answer. Each element in c will have the same
-	         * value as SIZE after the matmul operation.
-	         */
-            a[i][j] = 1.0;
-            b[i][j] = 1.0;
+             * the correct answer. Each element in c will have the same
+             * value as SIZE after the matmul operation.
+             */
+            a[row][j] = 1.0;
+            b[row][j] = 1.0;
         }
-}
-
-static void
-matmul_seq()
-{
-    int i, j, k;
-
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            c[i][j] = 0.0;
-            for (k = 0; k < SIZE; k++)
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
-        }
-    }
 }
 
 static void
@@ -80,17 +63,17 @@ void *child(void *id)
     int row = (int)id;
     printf("My row is %d\n", row);
     fflush(stdout);
+    init_matrix(row);
+    pthread_barrier_wait(&barrier);
     mulRow(row);
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    init_matrix();
-
     pthread_t *children;
     int id = 0;
-
+    pthread_barrier_init(&barrier,NULL,SIZE);
     children = malloc(SIZE * sizeof(pthread_t));
     for (id = 0; id < SIZE; id++)
     {
